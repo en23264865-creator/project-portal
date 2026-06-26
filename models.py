f1824b2d13a4a00007b811595cd572fe0a2e6f19
Ayesha Palekar <en23264865@git-india.edu.in>
2026-06-26 18:43:43 +0530

    Add contact field

--- models.py
+++ models.py
@@ -18,7 +18,8 @@ class User(db.Model):
     email         = db.Column(db.String(100), unique=True, nullable=False)
     password      = db.Column(db.String(255), nullable=False)
     role          = db.Column(db.String(50), nullable=False)
-    contact = db.Column(db.String(10), nullable=False)
+    contact       = db.Column(db.String(10), nullable=True)
+    firebase_uid  = db.Column(db.String(255), unique=True, nullable=True)
     # roles: Student | Guide | HoD | External Examiner
 
     # Guide-specific: up to 3 domains
