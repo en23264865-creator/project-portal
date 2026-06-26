01e60ea064a3663dee0267814bc7df7f2ba5f124
Ayesha Palekar <en23264865@git-india.edu.in>
2026-06-26 21:43:10 +0530

    g

--- seed.py
+++ seed.py
@@ -424,9 +424,9 @@ if __name__ == '__main__':
 
         from models import User, Project
         if Project.query.count() == 0:
-                print("🌱 Running seed...")
-                from seed import seed
-                seed()
+            print("🌱 Running seed...")
+            from seed import seed
+            seed()
 
             print("================================")
             print("Users:", User.query.count())
